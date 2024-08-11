1896f3e8be2816c13baa473bd2c376c6b4a02dad
jessesongok <jessesongok@gmail.com>
2024-08-11 09:55:13 +0000

    test

---
+++ Screens/HomeScreen.js
@@ -0,0 +1,11 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:4228583259.
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
