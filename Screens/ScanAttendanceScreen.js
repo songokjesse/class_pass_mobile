92c90d2593f5278ebede7e69628d737458787f88
Jesse Songok <jessesongok@gmail.com>
2024-08-13 12:12:26 +0300

    Feature: Camera Module

--- Screens/ScanAttendanceScreen.js
+++ Screens/ScanAttendanceScreen.js
@@ -1,26 +1,71 @@
-import {SafeAreaView, StyleSheet} from "react-native";
+import {Button, StyleSheet, View, Text} from "react-native";
 import {CameraView, useCameraPermissions} from "expo-camera";
 import {useState} from "react";
 
 export  default function (){
-    const [facing, setFacing] = useState('back');
     const [permission, requestPermission] = useCameraPermissions();
+    const [scannedData, setScannedData] = useState(null);
+    // const [scannedData, setScannedData] = useState<string | null>null; // Explicitly type scannedData as string or null
+
+    if (!permission) {
+        // Camera permissions are still loading.
+        return <View />;
+    }
+
+    if (!permission.granted) {
+        // Camera permissions are not granted yet.
+        return (
+            <View style={styles.permission_container}>
+                <Text style={styles.message}>We need your permission to show the camera</Text>
+                <Button onPress={requestPermission} title="grant permission" />
+            </View>
+        );
+    }
+    async function onBarCodeScanned({ data }) {
+        setScannedData(data);
+        console.log(data);
+    }
     return(
-        <SafeAreaView style={styles.container}>
-            <CameraView style={styles.camera} facing={facing}>
+        <View style={styles.container}>
+        <CameraView
+                style={StyleSheet.absoluteFill}
+                facing="back"
+                barcodeScannerSettings={{
+                    barcodeTypes: ["qr"],
+                }}
+                onBarCodeScanned={onBarCodeScanned}
+            />
+            <View style={styles.border}>
+                {/* You can customize the border styles here */}
+            </View>
 
-
-            </CameraView>
-        </SafeAreaView>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    camera: {
-        flex: 1,
+    permission_container:{
+        padding: 20,
+        rowGap:16
     },
     container: {
         flex: 1,
         justifyContent: 'center',
+        alignItems: 'center',
+    },
+    border: {
+        position: 'absolute',
+        width: 200,
+        height: 200,
+        borderWidth: 2,
+        borderColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    scannedDataText: {
+        position: 'absolute',
+        bottom: 20,
+        color: 'white',
     },
 })
+
