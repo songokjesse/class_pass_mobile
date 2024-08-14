cc7fbe04bd61e7c920a2959e609ab932c0f2e0a8
Jesse Songok <jessesongok@gmail.com>
2024-08-14 20:40:30 +0300

    Refactor: Registration and QR Scanner Formatter

--- Screens/RegisterScreen.js
+++ Screens/RegisterScreen.js
@@ -1,107 +1,113 @@
-import { SafeAreaView, View, Image, Button, Platform, StyleSheet } from "react-native";
-import { useState, useContext} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Image,
+  Button,
+  Platform,
+  StyleSheet,
+} from "react-native";
+import { useState, useContext } from "react";
 import FormTextField from "../components/FormTextField";
-import {loadUser, register} from "../services/AuthService";
+import { loadUser, register } from "../services/AuthService";
 import AuthContext from "../context/AuthContext";
-import logo from '../assets/logo.png'; // Import image
+import logo from "../assets/logo.png"; // Import image
 
 export default function () {
-    const {setUser} = useContext(AuthContext);
-    const [name, setName] = useState('');
-    const [admissionNumber, setAdmissionNumber] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordConfirmation, setPasswordConfirmation] = useState('');
-    const [errors, setErrors] = useState({});
-    const handleRegister = async () => {
-        setErrors({});
-        try {
-             await register({
-                 name,
-                email,
-                 admission_number: admissionNumber,
-                password,
-                 password_confirmation: passwordConfirmation,
-                device_name: `${Platform.OS} ${Platform.Version}`
-            })
+  const { setUser } = useContext(AuthContext);
+  const [name, setName] = useState("");
+  const [admissionNumber, setAdmissionNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [errors, setErrors] = useState({});
+  const handleRegister = async () => {
+    setErrors({});
+    try {
+      await register({
+        name,
+        email,
+        admission_number: admissionNumber,
+        password,
+        password_confirmation: passwordConfirmation,
+        device_name: `${Platform.OS} ${Platform.Version}`,
+      });
 
-            const user = await loadUser();
-             setUser(user);
-            console.log(user);
-        } catch (error) {
-            console.error('Registration failed:', error.response?.data);
-            if (error.response?.status === 422) {
-                setErrors(error.response.data.errors);
-            } else {
-                // Handle other errors (e.g., network issues)
-                console.error('Network or other error:', error);
-            }
-        }
+      const user = await loadUser();
+      setUser(user);
+      console.log(user);
+    } catch (error) {
+      console.error("Registration failed:", error.response?.data);
+      if (error.response?.status === 422) {
+        setErrors(error.response.data.errors);
+      } else {
+        // Handle other errors (e.g., network issues)
+        console.error("Network or other error:", error);
+      }
     }
-    return (
-        <SafeAreaView style={styles.wrapper}>
-            <Image source={logo} style={styles.image} />  
-            <View style={styles.container}>
-                <FormTextField
-                    label="Full Name"
-                    placeholder="Foobar"
-                    value={name}
-                    onChangeText={text => setName(text)}
-                    errors={errors.name}
-                />
-                <FormTextField
-                    label="Email Address"
-                    value={email}
-                    onChangeText={text => setEmail(text)}
-                    keyboardType="email-address"
-                    errors={errors.email}
-                />
-                <FormTextField
-                    label="Admission Number"
-                    value={admissionNumber}
-                    onChangeText={text => setAdmissionNumber(text)}
-                    errors={errors.admission_number}
-                />
-                <FormTextField
-                    label="Password"
-                    value={password}
-                    onChangeText={text => setPassword(text)}
-                    secureTextEntry={true}
-                    errors={errors.password}
-                />
-                <FormTextField
-                    label="Password Confirmation"
-                    value={passwordConfirmation}
-                    onChangeText={text => setPasswordConfirmation(text)}
-                    secureTextEntry={true}
-                    errors={errors.password_confirmation}
-                />
-                <Button title="Create Account" onPress={handleRegister} />
-
-            </View>
-        </SafeAreaView>
-    )
+  };
+  return (
+    <SafeAreaView style={styles.wrapper}>
+      <Image source={logo} style={styles.image} />
+      <View style={styles.container}>
+        <FormTextField
+          label="Full Name"
+          placeholder="Foobar"
+          value={name}
+          onChangeText={(text) => setName(text)}
+          errors={errors.name}
+        />
+        <FormTextField
+          label="Email Address"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          keyboardType="email-address"
+          errors={errors.email}
+        />
+        <FormTextField
+          label="Admission Number"
+          value={admissionNumber}
+          onChangeText={(text) => setAdmissionNumber(text)}
+          errors={errors.admission_number}
+        />
+        <FormTextField
+          label="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry={true}
+          errors={errors.password}
+        />
+        <FormTextField
+          label="Password Confirmation"
+          value={passwordConfirmation}
+          onChangeText={(text) => setPasswordConfirmation(text)}
+          secureTextEntry={true}
+          errors={errors.password_confirmation}
+        />
+        <Button title="Create Account" onPress={handleRegister} />
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    wrapper: { 
-        margin: '3%',
-        backgroundColor: "#fff", 
-        flex: 1,
-    },
-    container: {
-        marginTop: 'auto',
-        marginBottom: 'auto',
-        padding: 20, 
-        rowGap:16,
-    },
-    image: {
-        width: 80, // Adjust image width as needed
-        height: 80, // Adjust image height as needed
-        resizeMode: 'contain', // Adjust image scaling as needed
-        display: 'block',
-        // marginTop: 10,
-        marginLeft: 'auto',
-        marginRight: 'auto',  
-      },
-})
+  wrapper: {
+    margin: "3%",
+    backgroundColor: "#fff",
+    flex: 1,
+  },
+  container: {
+    marginTop: "auto",
+    marginBottom: "auto",
+    padding: 20,
+    rowGap: 16,
+  },
+  image: {
+    width: 80, // Adjust image width as needed
+    height: 80, // Adjust image height as needed
+    resizeMode: "contain", // Adjust image scaling as needed
+    display: "block",
+    // marginTop: 10,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+});
--- Screens/ScanAttendanceScreen.js
+++ Screens/ScanAttendanceScreen.js
@@ -1,93 +1,88 @@
+import { Button, StyleSheet, View, Text, Alert } from "react-native";
+import { CameraView, useCameraPermissions } from "expo-camera";
+// import { useContext, useState } from "react";
+// import AuthContext from "../context/AuthContext"; // Import the AuthContext
 
-import {Button, StyleSheet, View, Text, Vibration, Alert} from "react-native";
-import {CameraView, useCameraPermissions} from "expo-camera";
-import {useContext, useState} from "react";
-import AuthContext  from "../context/AuthContext"; // Import the AuthContext
+export default function App() {
+  const [permission, requestPermission] = useCameraPermissions();
+  const [scanned, setScanned] = useState(true);
+  const [data, setData] = useState(null);
+  // const { user } = useContext(AuthContext); // Get the user object from the AuthContext
 
-export default function App(){
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
 
-    const [permission, requestPermission] = useCameraPermissions();
-    const [scanned, setScanned] = useState(true);
-    const [data, setData] = useState(null);
-    const { user } = useContext(AuthContext); // Get the user object from the AuthContext
-
-    if (!permission) {
-        // Camera permissions are still loading.
-        return <View />;
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.permission_container}>
+        <Text style={styles.message}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+  async function onBarcodeScanned({ data }) {
+    if (!scanned) return;
+    try {
+      setScanned(false);
+      setData(data);
+      console.log(data);
+      Alert.alert("Attendance", "Attendance Captured Successfully");
+      // // Make a POST request to your API endpoint
+      // try {
+      //     const response = await axios.post('https://your-api-endpoint.com/qr-code-data', {
+      //         timetable_id: data,
+      //         user_id: user.id, // Include the user's ID in the request
+      //     });
+      //     console.log(response.data);
+      // } catch (error) {
+      //     console.error(error);
+      // }
+    } catch (e) {
+      setScanned(true);
+      console.log(e);
     }
+  }
 
-    if (!permission.granted) {
-        // Camera permissions are not granted yet.
-        return (
-            <View style={styles.permission_container}>
-                <Text style={styles.message}>We need your permission to show the camera</Text>
-                <Button onPress={requestPermission} title="grant permission" />
-            </View>
-        );
-    }
-    async function onBarcodeScanned({ data }) {
-        if(!scanned) return;
-        try {
-            setScanned(false);
-            setData(data);
-            console.log(data)
-            Alert.alert("Attendance", 'Attendance Captured Successfully')
-            // // Make a POST request to your API endpoint
-            // try {
-            //     const response = await axios.post('https://your-api-endpoint.com/qr-code-data', {
-            //         qrCodeData: data,
-            //         userId: user.id, // Include the user's ID in the request
-            //     });
-            //     console.log(response.data);
-            // } catch (error) {
-            //     console.error(error);
-            // }
-        } catch (e) {
-            setScanned(true);
-            console.log(e)
-        }
-    }
-
-    return(
-        <View style={styles.container}>
-            <CameraView
-                style={StyleSheet.absoluteFill}
-                facing="back"
-                barcodeScannerSettings={{
-                    barcodeTypes: ["qr"],
-                }}
-                onBarcodeScanned={onBarcodeScanned} // Ensure correct function reference
-            />
-            <View style={styles.border}>
-                {/* You can customize the border styles here */}
-            </View>
-            {scanned && (
-                <Text>
-                    Scanned data: {data}
-                </Text>
-            )}
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <CameraView
+        style={StyleSheet.absoluteFill}
+        facing="back"
+        barcodeScannerSettings={{
+          barcodeTypes: ["qr"],
+        }}
+        onBarcodeScanned={onBarcodeScanned} // Ensure correct function reference
+      />
+      <View style={styles.border}>
+        {/* You can customize the border styles here */}
+      </View>
+      {scanned && <Text>Scanned data: {data}</Text>}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    permission_container:{
-        padding: 20,
-        rowGap:16
-    },
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    border: {
-        position: 'absolute',
-        width: 200,
-        height: 200,
-        borderWidth: 2,
-        borderColor: 'white',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-})
-
+  permission_container: {
+    padding: 20,
+    rowGap: 16,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  border: {
+    position: "absolute",
+    width: 200,
+    height: 200,
+    borderWidth: 2,
+    borderColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
