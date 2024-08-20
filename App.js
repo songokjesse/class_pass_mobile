aa28f4b29abfc1afa40a028a4ddf52f1e827ede3
Jesse Songok <jessesongok@gmail.com>
2024-08-20 20:25:50 +0300

    Refactor: Splash Screen API

--- App.js
+++ App.js
@@ -5,30 +5,63 @@ import HomeScreen from "./Screens/HomeScreen";
 import AuthContext from "./context/AuthContext";
 
 import { loadUser } from "./services/AuthService";
-import { useState, useEffect } from "react";
-import SplashScreen from "./Screens/SplashScreen";
+import { useState, useEffect, useCallback } from "react";
 
 import RegisterScreen from "./Screens/RegisterScreen";
 import ScanAttendanceScreen from "./Screens/ScanAttendanceScreen";
 import ForgotPassword from "./Screens/ForgotPassword";
+import { SplashScreen } from "expo-splash-screen";
+
 const Stack = createNativeStackNavigator();
 export default function App() {
   const [user, setUser] = useState();
-  const [status, setStatus] = useState("Loading");
+  const [appIsReady, setAppIsReady] = useState(false);
+
   useEffect(() => {
-    async function runEffect() {
+    async function prepare() {
       try {
-        const user = await loadUser();
-        setUser(user);
+        // await Font.loadAsync({
+        //   InterBlack: require("node_modules/@expo-google-fonts/inter/Inter_900Black.ttf"),
+        // });
+        // Pre-load fonts, make any API calls you need to do here
+        // await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        // await new Promise((resolve) => setTimeout(resolve, 2000));
+
+        async function runEffect() {
+          try {
+            const user = await loadUser();
+            setUser(user);
+          } catch (e) {
+            console.log("Failed to load user", e);
+          }
+        }
+        await runEffect();
       } catch (e) {
-        console.log("Failed to load user", e);
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
       }
-      setStatus("idle");
     }
-    runEffect();
+
+    prepare();
   }, []);
-  if (status === "Loading") {
-    return <SplashScreen />;
+
+  useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
   return (
     <AuthContext.Provider value={{ user, setUser }}>
@@ -46,10 +79,7 @@ export default function App() {
             <>
               <Stack.Screen name="Login" component={LoginScreen} />
               <Stack.Screen name="Register" component={RegisterScreen} />
-              <Stack.Screen
-                name="ForgotPassword"
-                component={ForgotPassword}
-              />
+              <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
             </>
           )}
         </Stack.Navigator>
