a21ce9168bad905439a3bf724a98e0c87511ee24
Jesse Songok <jessesongok@gmail.com>
2024-09-12 11:23:25 +0300

    Refactor: Plaintext API

--- utils/axios.js
+++ utils/axios.js
@@ -2,8 +2,8 @@ import axiosLib from "axios";
 import { getToken } from "../services/TokenService";
 
 const axios = axiosLib.create({
-  // baseURL: "http://41.89.163.139/classpass/api",
-  baseURL: "https://classpass.vercel.app/api",
+  baseURL: "http://41.89.165.139/classpass/api",
+  // baseURL: "https://classpass.vercel.app/api",
   headers: {
     Accept: "application/json",
   },
